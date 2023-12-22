838da9690d75d3006e08de6914e3af84fd634157
Julian Bissantz <j.bissantz@gmail.com>
2023-12-22 16:28:48 -0600

    feat: Forward pass of LightningMLP is implemented and tested.

--- tests/test_lightning_mlp.py
+++ tests/test_lightning_mlp.py
@@ -1,8 +1,6 @@
-from __future__ import annotations
-
-from argparse import Namespace
-
 import pytest
+import torch
+from pytorch_lightning import seed_everything
 
 from yptl.models import LightningMLP
 
@@ -10,8 +8,13 @@ from yptl.models import LightningMLP
 @pytest.fixture()
 def hparams():
     activation = {"type": "ReLU", "args": {}}
-    return Namespace(n_inp=1, n_out=2, hidden_layers=[32, 32], activation=activation)
+    return {"n_inp": 2, "n_out": 2, "hidden_layers": [32, 32], "activation": activation}
 
 
 def test_create_model(hparams):
-    LightningMLP(**vars(hparams))
+    LightningMLP(**hparams)
+
+
+def test_run_forward_pass(hparams):
+    model = LightningMLP(**hparams)
+    assert torch.allclose(model.forward(torch.ones(1, 2)), torch.tensor([[0.14762419, 0.08727027]]))
